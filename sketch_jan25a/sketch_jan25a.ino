fe48447ba8f6dc2658c156cc094c769c072741df
Rocyon <161540596+rocyon@users.noreply.github.com>
2025-02-14 16:55:53 -0600

    Reverse Correction and Cleanup

--- sketch_jan25a/sketch_jan25a.ino
+++ sketch_jan25a/sketch_jan25a.ino
@@ -1,73 +1,54 @@
-
-
 #include <SoftwareSerial.h>
 #include <RoboClaw.h>
 
+#define address1 0x80
+#define address2 0x81
 
-SoftwareSerial serial(10, 11);  
-SoftwareSerial serial2(9, 8);   
-
-
-RoboClaw roboclaw(&serial, 10000);  
+SoftwareSerial serial1(10, 11);
+SoftwareSerial serial2(9, 8);
+RoboClaw roboclaw1(&serial1, 10000);
 RoboClaw roboclaw2(&serial2, 10000);
 
-
-#define address 0x80  
-#define address2 0x81  
-uint8_t data[4] = {0,0,0,0};
-
+uint8_t data[4] = { 0, 0, 0, 0 };
 
 void setup() {
-  
-  roboclaw.begin(38400);
+  roboclaw1.begin(38400);
   roboclaw2.begin(38400);
-  Serial.begin(9600);
 
+  // USB Serial
+  Serial.begin(9600);
 }
 
 void loop() {
-while (Serial.available()) {
-   Serial.readBytes(data, 4);
-}
+  while (Serial.available()) {
+    Serial.readBytes(data, 4);
+  }
 
-if ( data[0] >= 0) {
- roboclaw.ForwardM1(address, data[0]);
- Serial.write(data[0]);
- Serial.println(data[0]);
- 
-} else {
-  data[0] = -data[0];
-  roboclaw.BackwardM1(address, data[0]);
-}
+  // Echo the processed bytes
+  Serial.write(data, 4);
+  Serial.println();
 
-if ( data[1] >= 0) {
- roboclaw.ForwardM2(address, data[1]);
- Serial.write(data[1]);
- Serial.println(data[1]);
-} else {
-  data[1] = -data[1];
-  roboclaw.BackwardM2(address, data[1]);
-}
+  if (data[0] >= 0) {
+    roboclaw1.ForwardM1(address1, data[0]);
+  } else {
+    roboclaw1.BackwardM1(address1, 64 - data[0]);
+  }
 
-if ( data[2] >= 0) {
- roboclaw2.ForwardM1(address2, data[2]);
- Serial.write(data[2]);
- Serial.println(data[2]);
-} else {
-  data[2] = -data[2];
-  roboclaw2.BackwardM1(address2, data[2]);
-}
+  if (data[1] >= 0) {
+    roboclaw1.ForwardM2(address1, data[1]);
+  } else {
+    roboclaw1.BackwardM2(address1, 64 - data[1]);
+  }
 
-if ( data[3] >= 0) {
- roboclaw2.ForwardM2(address2, data[3]);
-  Serial.write(data[3]);
-  Serial.println(data[3]);
-} else {
-  data[3] = -data[3];
-  roboclaw2.BackwardM2(address2, data[3]);
-  
-}
+  if (data[2] >= 0) {
+    roboclaw2.ForwardM1(address2, data[2]);
+  } else {
+    roboclaw2.BackwardM1(address2, 64 - data[2]);
+  }
 
-Serial.println();
-
+  if (data[3] >= 0) {
+    roboclaw2.ForwardM2(address2, data[3]);
+  } else {
+    roboclaw2.BackwardM2(address2, 64 - data[3]);
+  }
 }
